f9e62007b0cc9d9757a597e1c6161765486a61c0
sangmin <sangmin5258@naver.com>
2021-09-17 14:28:09 +0900

    new vue-board project

---
+++ src/data/index.js
@@ -0,0 +1,18 @@
+export default[
+
+    {
+        write : '아이린',
+        title : '레드벨벳',
+        content : '1'
+    },
+    {
+        write : '슬기',
+        title : '레드벨벳2',
+        content : '2'
+    },
+    {
+        write : '웬디',
+        title : '레드벨벳3',
+        content : '3'
+    },
+]
