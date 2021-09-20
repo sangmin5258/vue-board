420c710b74e86178145116dfe7fd1e29e8e6abe4
sangmin <sangmin5258@naver.com>
2021-09-21 01:25:35 +0900

    updata vue-board

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -3,7 +3,7 @@
         <input v-model="writer" placeholder="글쓴이" />
         <input v-model="title" placeholder="제목" />
         <textarea v-model="content" placeholder="내용" />
-        <button @click="write">작성</button>
+        <button @click="index !== undefined ? updata() : write()">{{index !== undefined ? '수정' : '작성'}}</button>
     </div>
 </template>
 
@@ -13,11 +13,13 @@ import data from '@/data'
 export default{
     name: "Create",
     data() {
+        const index = this.$route.params.contentId;
         return {
             data: data,
-            writer: "",
-            title: "",
-            comtent: ""
+            index: index,
+            writer: index !== undefined ? data[index].writer : "",
+            title: index !== undefined ? data[index].title : "",
+            content:index !== undefined ? data[index].content : "",
         }
     },
     methods: {
@@ -27,6 +29,14 @@ export default{
                 title: this.title,
                 content: this.content,
             })
+            this.$router.push({
+                path: '/'
+            })
+        },
+        updata(){
+            data[this.index].writer = this.writer
+            data[this.index].title = this.title
+            data[this.index].content = this.content
             this.$router.push({
                 path: '/'
             })
--- src/data/index.js
+++ src/data/index.js
@@ -1,17 +1,17 @@
 export default[
 
     {
-        write : '아이린',
+        writer : '아이린',
         title : '레드벨벳',
         content : '1'
     },
     {
-        write : '슬기',
+        writer : '슬기',
         title : '레드벨벳2',
         content : '2'
     },
     {
-        write : '웬디',
+        writer : '웬디',
         title : '레드벨벳3',
         content : '3'
     },
